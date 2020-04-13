cd2ad0703ad56e17bedd09b5f94caab00abf109a
shoko.miyagawa <s.mygw@hitohineri.jp>
2020-04-13 16:55:03 +0900

    Feat: add create package.json command

---
+++ createPackageJson.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# PACKAGE=typeB
+echo $PACKAGE
+VER=`npm info @shokorep/$PACKAGE version` || VER=1.0.0
+echo set $PACKAGE version to $VER
+# cat docs/template.json | ver=123 envsubst 
+# cat docs/template.json | ver=123 envsubst 
+# echo $ver
+
+# cat sample2.json | ver=$ver envsubst > sample3.json
+# cat sample2.json | ver=$ver envsubst > sample3.json
+
+# ver=123
+sed -e "s/\$ver/$VER/g" -e "s/\$package/$PACKAGE/g" docs/template.json > docs/$PACKAGE/package.json
